9c9dee81de564c4da824388ff85595619961b09f
Oscar Enrique Marchena <omarchena@u1965.edu.do>
2019-04-09 18:41:44 -0400

    Add project files.

---
+++ xam/xam/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using xam.Services;
+using xam.Views;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Push;
+
+
+namespace xam
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            AppCenter.Start("1819574d-f2c6-4135-9a1d-f80cd118b87a", typeof(Push));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
